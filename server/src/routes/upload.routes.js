e0697568e7da5118faa8c37b99de213c564ce963
samir vanapariya <samir.vanpariya@trootech.com>
2026-06-03 17:27:49 +0530

    Fix: upload route with use of  common multer  config file

--- server/src/routes/upload.routes.js
+++ server/src/routes/upload.routes.js
@@ -1,12 +1,8 @@
 import express from "express";
-import multer from "multer";
+import upload from "../config/multer.js";
 import { uploadFile } from "../controllers/upload.controller.js";
 
 const router = express.Router();
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 router.post("/upload", upload.single("avatar"), uploadFile);
 
 export default router;
