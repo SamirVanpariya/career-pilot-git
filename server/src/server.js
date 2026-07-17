cd57d948924aaae9c82ad90920d1d1561a4cc71c
samir vanapariya <samir.vanpariya@trootech.com>
2026-07-17 10:54:02 +0530

    port change

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@ import http from "http";
 import app from "./app.js";
 import { initializeSocket } from "./socket.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || "5000";
 
 // Create HTTP server from Express app
 const server = http.createServer(app);
@@ -12,6 +12,6 @@ initializeSocket(server);
 
 // Start server
 server.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at ${PORT}`);
   console.log("🔌 Socket.IO initialized");
 });
